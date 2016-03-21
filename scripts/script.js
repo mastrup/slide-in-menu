f544c89ab4f74ae2fa59da32b327a938006b0a30
Søren Mastrup <sm@oerskov.dk>
2016-03-21 10:36:42 +0100

    Cleaned up script file

--- scripts/script.js
+++ scripts/script.js
@@ -7,41 +7,42 @@ $(function () {
 	});
 
 	$(".goto-submenu").click(function () {
-		$(this).siblings(".submenu").toggleClass("hidden");
-		//$(".active").removeClass("active").scrollTop(0);
 		$(".active").animate({
 			scrollTop: 0
-		}, 'slow').removeClass("active");
-		$(this).siblings(".submenu").toggleClass("active");
-		checkIfRoot($(this).parent().closest("ul"));
+		}, 'slow');
+		$(this).siblings(".submenu").toggleClass("active active-layer");
+		$(this).parent().closest("ul").removeClass("active-layer");
+		$(".mobile-navigation-back").removeClass("hidden")
 	});
 
 	$(".mobile-navigation-back").click(function () {
-		var old = $(".submenu.active");
-		old.removeClass("active").addClass("hidden");
-		old.parent().closest("ul").addClass("active");
-		checkIfRoot(old.parent().closest("ul"));
+		var element = $(".submenu.active-layer");
+		element.removeClass("active active-layer");
+		element.parent().closest("ul").addClass("active-layer");
+		if(!$(this).hasClass("hidden")){
+			checkIfRoot(element);
+		}
 	});
 
 	if($(".rootmenu").data("currentpage") != "" && $(".rootmenu").data("currentpage") != undefined){
 		var currentId = $(".rootmenu").data("currentpage");
 		var element = $("a[data-id='" + currentId + "']").closest(".submenu");
-		element.removeClass("hidden").addClass("active");
-		element.parents(".submenu").removeClass("hidden")
+		element.addClass("active active-layer");
+		element.parents(".submenu").addClass("active");
 		if(element.length){
-			checkIfRoot(element.parent().closest("ul"));
+			checkIfRoot(element);
 		}
 	}
-
-	function checkIfRoot(parentMenu){
-		if(parentMenu.hasClass("rootmenu active")){
+	
+	window.addEventListener('orientationchange', setMenuHeight);
+	
+	function checkIfRoot(element){
+		if(element.parent().closest("ul").hasClass("rootmenu")){
 			$(".mobile-navigation-back").addClass("hidden");
 		}else{
 			$(".mobile-navigation-back").removeClass("hidden");
 		}
 	}
-
-	window.addEventListener('orientationchange', setMenuHeight);
 
 	function setMenuHeight(){
 		var containerHeight = $("#mobile-navigation-container").outerHeight();
